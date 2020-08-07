68349c4f8db910d2bcb6628750672d2249cf4e93
Christoph1911 <68999214+Christoph1911@users.noreply.github.com>
2020-08-08 00:19:15 +0200

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -62,8 +62,8 @@ class Main extends React.Component {
             <img src={contact} alt="" />
           </span>
           <p>
-      <a href="mailto:christophpaltzer@gmail.com" style={{margin: auto}} ><img src={Mailicon} alt="" /></a>
-      <a href="https://github.com/Christoph1911" style={{margin: auto}} ><img src={Githubicon} alt="" /></a>
+      <a href="mailto:christophpaltzer@gmail.com" style={{margin-left: 100px}} ><img src={Mailicon} alt="" /></a>
+      <a href="https://github.com/Christoph1911" style={{margin-right: 100px}} ><img src={Githubicon} alt="" /></a>
       </p>
           {close}
         </article>
