837e781e4f0343388dc27c91200869ff97eae2a0
Christoph1911 <christophpaltzer@gmail.com>
2020-08-07 18:02:28 +0200

    logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import about from '../images/about.jpg'
 import contact from '../images/contact.jpg'
-import Githubicon from '../images/GitHub-icon.png'
+import Githubicon from '../images/log-2.png'
+import Mailicon from '../images/email.png'
+
 
 class Main extends React.Component {
   render() {
@@ -60,11 +62,11 @@ class Main extends React.Component {
             <img src={contact} alt="" />
           </span>
           <p>
-      <a href="mailto:christophpaltzer@gmail.com"><img src={Githubicon} alt="" /></a>
+      <a href="mailto:christophpaltzer@gmail.com"><img src={Mailicon} alt="" /></a>
       
       </p>
       <p>
-        <a href="https://github.com/Christoph1911">Github</a>
+        <a href="https://github.com/Christoph1911">img src={Githubicon} alt="" /></a>
       </p>
           {close}
         </article>
