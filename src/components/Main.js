c7ecaab69baf37767e3ba93526400a515ba075f5
Christoph1911 <christophpaltzer@gamil.com>
2021-11-04 11:20:57 +0100

    c

--- src/components/Main.js
+++ src/components/Main.js
@@ -62,18 +62,7 @@ class Main extends React.Component {
             <img src={about} alt="" />
           </span>
           <p>
-        Currently I'm in high school in Germany and go in the 12th grade.
-        I've done a one week internship at <a href="https://flowkey.breezy.hr">flowkey</a>, a startup in Berlin, in the development department. It gave me a detailed insight into the professional field of a software engineer.
-        I also built this website, learned the basics of machine learning and other things.
-      </p>
-      <p>
-        I'm interested in programming and have tried a few different programming languages: C#, Java, Javascript, Python. I got into programming in school in the 8th grade, where I started experimenting with the Arduino.
-        In the 11th grade I learned the basics of Java.
-        Lately I've had a lot of fun with game development. <a href="https://christoph191103.itch.io">Here</a> you can have a look at a little game I created with C# and Unity.
-      </p>
-      <p>
-        Other hobbys of mine are table tennis, Karate, playing the saxophone and chess.
-        I also enjoy speedcubing. You try to solve the Rubik's cube and other puzzles as quickly as possible. Competitions are held all over the world. <a href="https://www.worldcubeassociation.org/persons/2017PALT01">Here</a> you can view my WCA profile.
+            Here I am documenting my projects.
       </p>
           {close}
         </article>
